4e6165a67fd9c46b4faafd4c660aedd5aabc4812
Paul MICHAU <paulmichau@outlook.fr>
2023-11-24 17:32:53 +0100

    Revert des modifications de main.py

--- main.py
+++ main.py
@@ -6,16 +6,3 @@ import time
 import robot_distance_actions
 import robot_move_actions_unblocked
 import robot_follow_personn
-if __name__ == '__main__':
-    
-    ep_robot = robot.Robot()
-    ep_robot.initialize(conn_type="ap")    
-    ep_robot.set_robot_mode(mode=robot.CHASSIS_LEAD)
-    print('initialized')
-    while input('Ready ? ')!='yes':
-        print('Ok, waiting...')
-        pass
-    print('Try to follow')
-    print(robot_follow_personn.follow_personn())
-    time.sleep(60)
-    ep_robot.close()
