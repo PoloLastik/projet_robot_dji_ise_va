1c544c37d424e6d40e0710152cb5ae6de216604b
Paul MICHAU <paulmichau@outlook.fr>
2023-11-26 14:39:22 +0100

    Ajout de robot_move_actions des param supp

--- robot_move_actions.py
+++ robot_move_actions.py
@@ -10,5 +10,10 @@ def robot_stop(robot, X, Y, Z):
     ep_chassis = robot.chassis
     ep_chassis.drive_speed(x=-X, y=-Y, z=-Z, timeout=5)
     time.sleep(0.1)
-    ep_chassis.drive_speed(x=0, y=0, z=0, timeout=5)
+    ep_chassis.drive_speed(x=0, y=0, z=0, timeout=1)
+    time.sleep(1)
+
+def robot_distance_move(robot, x=0 , y=0 , z=0 , xy_speed=0.5 , z_speed=30):
+    driving_action = robot.chassis.move(x,y,z,xy_speed,z_speed)
+    driving_action.wait_for_completed()
     
